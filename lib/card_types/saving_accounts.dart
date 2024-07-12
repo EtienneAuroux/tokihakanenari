bff50d79d8c1981e740dac02bd41ac2e54d6afd4
EtienneAuroux-ImpulseRadar <132366116+EtienneAuroux-ImpulseRadar@users.noreply.github.com>
2024-07-12 12:03:45 +0200

    <feat>: Started to work on big card content for SavingAccounts.

--- lib/card_types/saving_accounts.dart
+++ lib/card_types/saving_accounts.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tokihakanenari/my_enums.dart';
+import 'package:tokihakanenari/visual_tools/card_decoration.dart';
 import 'package:tokihakanenari/visual_tools/text_styles.dart';
+import 'dart:developer' as developer;
 
 class SavingAccounts extends StatefulWidget {
   final CardSize cardSize;
@@ -15,14 +17,139 @@ class SavingAccounts extends StatefulWidget {
 }
 
 class _SavingAccountsState extends State<SavingAccounts> {
-  Widget getCardContent(CardSize cardStatus) {
-    switch (cardStatus) {
-      case CardSize.big:
-        return const Center(
-          child: Text(
-            'Saving accounts',
+  late List<Widget> savingAccounts;
+  int numberOfSavingAccounts = 0;
+
+  List<Widget> initializeSavingAccounts() {
+    return <Widget>[
+      const Text(
+        'Saving accounts',
+        style: TextStyles.bigCardTitle,
+      ),
+      const Icon(Icons.new_label),
+    ];
+  }
+
+  // void addSavingAccount() {
+  //   savingAccounts.insert(
+  //       savingAccounts.length - 1,
+  //       const Row(
+  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //         crossAxisAlignment: CrossAxisAlignment.center,
+  //         children: [
+  //           Icon(Icons.abc),
+  //           Text(
+  //             'amount',
+  //             style: TextStyles.bigCardText,
+  //           ),
+  //           Text(
+  //             'rate',
+  //             style: TextStyles.bigCardText,
+  //           ),
+  //         ],
+  //       ));
+  //   setState(() {});
+  // }
+
+  // Future<void> alertPopup(String notification, {bool forceConnection = false}) async {
+  //   return showDialog<void>(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return AlertDialog(
+  //         title: const Text('Connection issue:'),
+  //         content: SingleChildScrollView(
+  //           child: ListBody(
+  //             children: <Widget>[
+  //               Text(notification),
+  //             ],
+  //           ),
+  //         ),
+  //         actions: forceConnection
+  //             ? <Widget>[
+  //                 TextButton(
+  //                   child: const Text('Yes'),
+  //                   onPressed: () {
+  //                     // connect(forceConnection: true);
+  //                     Navigator.of(context).pop();
+  //                   },
+  //                 ),
+  //                 TextButton(
+  //                   child: const Text('No'),
+  //                   onPressed: () {
+  //                     Navigator.of(context).pop();
+  //                   },
+  //                 ),
+  //               ]
+  //             : <Widget>[
+  //                 TextButton(
+  //                   child: const Text('Ok'),
+  //                   onPressed: () {
+  //                     Navigator.of(context).pop();
+  //                   },
+  //                 ),
+  //               ],
+  //       );
+  //     },
+  //   );
+  // }
+
+  Future<void> savingAccountDialog(int index, int numberOfSavingAccounts) {
+    Widget content;
+    if (index == 0) {
+      content = const Text('Display total');
+    } else if (index == numberOfSavingAccounts + 1) {
+      content = Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'New account:',
             style: TextStyles.bigCardTitle,
           ),
+          const Row(
+            children: [
+              Icon(Icons.ac_unit_outlined),
+              Icon(Icons.access_alarms),
+            ],
+          ),
+          const Text('amount'),
+          const Text('rate'),
+          TextButton(onPressed: () {}, child: const Icon(Icons.send)),
+        ],
+      );
+    } else {
+      content = const Text('Modify account');
+    }
+
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: DecoratedBox(
+              decoration: CardDecoration.getMiniDecoration(CardType.savingAccounts),
+              child: content,
+            ),
+            contentPadding: const EdgeInsets.all(0),
+          );
+        });
+  }
+
+  Widget getCardContent(CardSize cardStatus, BuildContext context) {
+    switch (cardStatus) {
+      case CardSize.big:
+        return ListView.builder(
+          itemCount: savingAccounts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+              child: TextButton(
+                onPressed: () {
+                  savingAccountDialog(index, numberOfSavingAccounts);
+                },
+                child: savingAccounts[index],
+              ),
+            );
+          },
         );
       case CardSize.mini:
         return const Center(
@@ -43,7 +170,14 @@ class _SavingAccountsState extends State<SavingAccounts> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    savingAccounts = initializeSavingAccounts();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return getCardContent(widget.cardSize);
+    return getCardContent(widget.cardSize, context);
   }
 }
--- lib/visual_tools/text_styles.dart
+++ lib/visual_tools/text_styles.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 
 class TextStyles {
   static const bigCardTitle = TextStyle(
+    fontSize: 30,
+    color: Colors.black,
+    decoration: TextDecoration.none,
+  );
+
+  static const bigCardText = TextStyle(
     fontSize: 20,
     color: Colors.black,
     decoration: TextDecoration.none,
   );
+
   static const miniCardTitle = TextStyle(
     fontSize: 20,
     color: Colors.black,
     decoration: TextDecoration.none,
   );
+
   static const smallCardTitle = TextStyle(
     fontSize: 20,
     color: Colors.black,
